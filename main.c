4825c3c07069e48fe3308e206db91fa277c04abe
Marvin Sevilla <mpsevilla@cpp.edu>
2021-12-02 08:08:25 +0000

    Finished second part of 'Reading a line' section

--- main.c
+++ main.c
@@ -65,3 +65,21 @@ char *lsh_read_line(void)
 		}
 	}
 }
+
+char *lsh_read_line(void)
+{
+	char *line = NULL;
+	ssize_t bufsize = 0; // have getline allocate a buffer for us 
+
+	if (getline(&line, &bufsize, stdin) == -1)
+	{
+		if (feof(stdin)) {
+			exit(EXIT_SUCCESS);
+		}else{
+			perror("readline");
+			exit(EXIT_FAILURE);
+		}
+	}
+	
+	return line;
+}
