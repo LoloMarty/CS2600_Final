88746012066f2be49a2199bc88011e56613175e8
Marvin Sevilla <mpsevilla@cpp.edu>
2021-12-02 08:56:20 +0000

    Finished section 'Putting together builtins and processes'

--- main.c
+++ main.c
@@ -218,5 +218,25 @@ int lsh_exit(char **args)
 	return 0;
 }
 
+//Section : Putting together builtins and processes
 
+int lsh_execute(har **args)
+{
+	int i;
 
+	if (args[0] == NULL)
+	{
+		//An empty command was entered
+		return 1;
+	}
+
+	for (i = 0; i<lsh_num_builtins(); i++)
+	{
+		if (strcmp(args[0], builtin_str[i]) == 0)
+		{
+			return (*builtin_func[i])(args);
+		}
+	}
+
+	return lsh_launch(args);
+}
