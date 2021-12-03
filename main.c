3b0505ad4a5892c5ae3672c859b6ff7813c44682
Marvin Sevilla <mpsevilla@cpp.edu>
2021-12-03 08:23:17 +0000

    Finished reorganizing code

--- main.c
+++ main.c
@@ -4,104 +4,118 @@
 #include <stdio.h>
 #include <string.h>
 
-//Section: Shell Builtins
 
+//Shell builtins
 /*
-        Function declarations for builtin shell commands:
+	Function declarations for builtin shell commands:
 */
 int lsh_cd(char **args);
 int lsh_help(char **args);
 int lsh_exit(char **args);
 
 /*
-        List of builtin commands, followed by their corresponding functions
+	List of builtin commands, followed by their corresponding functions
 */
 
 char *builtin_str[] = {
-        "cd",
-        "help",
-        "exit"
+	"cd",
+	"help",
+	"exit"
 };
+
 
 int (*builtin_func[]) (char **) = {
-        &lsh_cd,
-        &lsh_help,
-        &lsh_exit
+	&lsh_cd,
+	&lsh_help,
+	&lsh_exit
 };
 
-int lsh_num_builtins()
-{
-        return sizeof(builtin_str) / sizeof(char *);
+int lsh_num_builtins() {
+	return sizeof(builtin_str) / sizeof(char *);
 }
 
+//Shell Builtins
 /*
-        Builtin function implementations
+	Function declarations for builtin shell commands:
 */
 
 int lsh_cd(char **args)
 {
-        if (args[1] == NULL)
-        {
-                fprintf(stderr, "lsh: expected arguement to \"cd\"\n");
-        }else{
-                if (chdir(args[1]) != 0)
-                {
-                        perror("lsh");
-                }
-        }
-        return 1;
+	if (args[1] == NULL) {
+		fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+	} else {
+		if (chdir(args[1]) != 0) {
+			perror("lsh");
+		}
+	}
+	return 1;
 }
 
 int lsh_help(char **args)
 {
-        int i;
-        printf("Marvin Sevilla's LSH\n"); //originally Stephen Brennan's instead of Marvin Sevilla's
-        printf("Type program names and arguments, and hit enter.\n");
-        printf("The following are buult in:\n");
+	int i;
+	printf("Marvin Sevilla's LSH\n"); //originally Stephen Brennan's instead of Marvin Sevilla's
+	printf("Type program names and arguments, and hit enter.\n");
+	printf("The following are built in:\n");
 
-        for (i=0; i<lsh_num_builtins(); i++)
-        {
-                printf("        %s\n", builtin_str[i]);
-        }
+	for (i = 0; i < lsh_num_builtins(); i++) {
+		printf("  %s\n", builtin_str[i]);
+	}
 
-        printf("Use the man command for information on other programs.\n");
-        return 1;
+	printf("Use the man command for information on other programs.\n");
+	return 1;
 }
 
 int lsh_exit(char **args)
 {
-        return 0;
+	return 0;
+}
+
+//Section : How shells start processes
+int lsh_launch(char **args)
+{
+	pid_t pid, wpid;
+	int status;
+
+	pid = fork();
+	if (pid == 0) {
+  		// Child process
+  		if (execvp(args[0], args) == -1) {
+    			perror("lsh");
+		}
+		exit(EXIT_FAILURE);
+	} else if (pid < 0) {
+		// Error forking
+		perror("lsh");
+	} else {
+		// Parent process
+		do {
+			wpid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	return 1;
 }
 
 
+//Section : Putting together builtins and processes
 
-//Section : Basic lifetime of a shell
-int main (int argc, char **argv)
+int lsh_execute(char **args)
 {
-	//run command loop
-	lsh_loop();
+	int i;
 
-	//performing shutdown/cleanup
-	return EXIT_SUCCESS;
-}
+	if (args[0] == NULL) {
+		// An empty command was entered.
+		return 1;
+	}
 
+	for (i = 0; i < lsh_num_builtins(); i++) {
+		if (strcmp(args[0], builtin_str[i]) == 0) {
+			return (*builtin_func[i])(args);
+		}
+	}
 
-//Section : Basic loop of a shell
-void lsh_loop(void)
-{
-	char *line;
-	char **args;
-	int status; 
-
-	do {
-		printf("> ");
-		line = lsh_read_line();
-		args = lsh_split_line(line);
-		status = lsh_execute(args);
-
-		free(line);
-		free(args);
-	} while (status);
+	return lsh_launch(args);
 }
 
 //Section : Reading a line 
@@ -115,14 +129,14 @@ char *lsh_read_line(void)
 
 	if (!buffer) {
 		fprintf(stderr, "lsh: allocation error\n");
-		exit(EXIT_FAILURE);
+	exit(EXIT_FAILURE);
 	}
 
-	while (1){
-		//Read a character	
+	while (1) {
+		//read a character
 		c = getchar();
 
-		// if we hit the end of file, replace it with a null character and return 
+		// if we hit EOF, replace it with a null character and return.
 		if (c == EOF || c == '\n') {
 			buffer[position] = '\0';
 			return buffer;
@@ -131,7 +145,7 @@ char *lsh_read_line(void)
 		}
 		position++;
 
-		//if we've exceeded the buffer, reallocate
+		// If we have exceeded the buffer, reallocate.
 		if (position >= bufsize) {
 			bufsize += LSH_RL_BUFSIZE;
 			buffer = realloc(buffer, bufsize);
@@ -145,88 +159,66 @@ char *lsh_read_line(void)
 
 //Section : Parsing the lines
 #define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM "		\t\r\n\a"
+#define LSH_TOK_DELIM " \t\r\n\a"
 char **lsh_split_line(char *line)
 {
 	int bufsize = LSH_TOK_BUFSIZE, position = 0;
 	char **tokens = malloc(bufsize * sizeof(char*));
 	char *token;
 
-	if (!tokens) 
-	{
+	if (!tokens) {
 		fprintf(stderr, "lsh: allocation error\n");
 		exit(EXIT_FAILURE);
 	}
 
 	token = strtok(line, LSH_TOK_DELIM);
-	while (token != NULL)
-	{
+	while (token != NULL) {
 		tokens[position] = token;
 		position++;
 
-		if (position >= bufsize)
-		{
+		if (position >= bufsize) {
 			bufsize += LSH_TOK_BUFSIZE;
 			tokens = realloc(tokens, bufsize * sizeof(char*));
-			if (!tokens)
-			{
+			if (!tokens) {
 				fprintf(stderr, "lsh: allocation error\n");
 				exit(EXIT_FAILURE);
 			}
 		}
+
 		token = strtok(NULL, LSH_TOK_DELIM);
 	}
 	tokens[position] = NULL;
-	return tokens; 
+	return tokens;
 }
 
-//Section : How shells start processes
-int lsh_launch(char **args)
+//Section : Basic loop of a shell
+void lsh_loop(void)
 {
-	pid_t pid, wpid;
+	char *line;
+	char **args;
 	int status;
 
-	pid = fork();
-	if (pid == 0)
-	{
-		//child process
-		if (execvp(args[0], args) == -1)
-		{
-			perror("lsh");
-		}
-		exit(EXIT_FAILURE);
-	} else if (pid < 0) {
-		//error forking 
-		perror("lsh");
-	}else{
-		//parent process
-		do {
-			wpid = waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-	}
+	do {
+		printf("> ");
+		line = lsh_read_line();
+		args = lsh_split_line(line);
+		status = lsh_execute(args);
 
-	return 1;
+		free(line);
+		free(args);
+	} while (status);
 }
 
-//Section : Putting together builtins and processes
 
-int lsh_execute(char **args)
+//Section : Basic lifetime of a shell
+int main(int argc, char **argv)
 {
-	int i;
 
-	if (args[0] == NULL)
-	{
-		//An empty command was entered
-		return 1;
-	}
+	// Run command loop.
+	lsh_loop();
 
-	for (i = 0; i<lsh_num_builtins(); i++)
-	{
-		if (strcmp(args[0], builtin_str[i]) == 0)
-		{
-			return (*builtin_func[i])(args);
-		}
-	}
+	// Perform any shutdown/cleanup.
 
-	return lsh_launch(args);
+	return EXIT_SUCCESS;
 }
+
