c60e5f03b4c42b69ad13725108da6619e8bb313a
Marvin Sevilla <mpsevilla@cpp.edu>
2021-12-04 23:11:04 +0000

    Finished Section 2, Disable Raw Mode at Exit and Turn off Conical Mode

--- kilo.c
+++ kilo.c
@@ -1,18 +1,26 @@
-
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-void enableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+struct termios orig_termios;
+
+void disableRawMode() {
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
 
+void enableRawMode() {
+	tcgetattr(STDIN_FILENO, &orig_termios);
+	atexit(disableRawMode);
+
+	struct termios raw = orig_termios;
+	raw.c_lflag &= ~(ECHO | ICANON);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
-  enableRawMode();
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-  return 0;
+	enableRawMode();
+	char c;
+	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+	return 0;
 }
